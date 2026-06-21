8ee5abb297ea62785678e82aeac9338a5b408634
ChessWize Coder <support@chesswizepro.com>
2026-06-21 13:57:27 +0530

    Configure Zoom Server-to-Server OAuth credentials

--- zoom-config.php
+++ zoom-config.php
@@ -2,6 +2,6 @@
 // Zoom Server-to-Server OAuth Credentials for Paras Chess Academy
 // Get these credentials by creating a "Server-to-Server OAuth" App in the Zoom App Marketplace (marketplace.zoom.us)
 
-define('ZOOM_ACCOUNT_ID', 'YOUR_ZOOM_ACCOUNT_ID');
-define('ZOOM_CLIENT_ID', 'YOUR_ZOOM_CLIENT_ID');
-define('ZOOM_CLIENT_SECRET', 'YOUR_ZOOM_CLIENT_SECRET');
+define('ZOOM_ACCOUNT_ID', 'cXbl5LNpQyWfticpPJSK4g');
+define('ZOOM_CLIENT_ID', '36qR8WbyQBi189vFxJkPfg');
+define('ZOOM_CLIENT_SECRET', 'sxDZ7Tstsq9woJt0NA5JpwLQUCxXZdhT');
